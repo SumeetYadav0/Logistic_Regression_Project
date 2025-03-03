891ca027534334219d5939a206164959d305c39e
Sumeet Yadav <Sumeetyadav.official@gmail.com>
2025-03-03 17:03:58 +0530

    data ingestion

--- setup.py
+++ setup.py
@@ -1,8 +1,28 @@
-from setuptools import find_packages, setup
+from setuptools import setup, find_packages
+from typing import List
 
+
+HYPEN_DOT_E = '-e.'
+
+def get_requirements(file_path:str)->list[str]:
+
+    requirements= []
+
+    with open(file_path) as file_obj:
+        requirements= file_obj.readlines()
+        requirements= [req.replace('\n',"") for req in requirements]
+
+        if HYPEN_DOT_E in requirements:
+         requirements.remove(HYPEN_DOT_E)
+
+    return requirements
 
 
 setup(
-    name= 'Logistic_Regression_Project'
-    
+name= 'mlproject1',
+version='0.0.1',
+author= 'Sumeet Yadav',
+author_email= 'sumeetyadav.official@gmail.com',
+packages= find_packages(),
+install_requires= get_requirements('requirements.txt')
 )
